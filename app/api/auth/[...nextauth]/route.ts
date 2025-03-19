1e3bc8055fb83fe10d3ac02fc76a00141564ab64
sakshi <sakshi.ag09@gmail.com>
2025-03-19 11:38:05 +0530

    fixed the auth issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,20 @@
-import { handlers } from "@/auth"
+import { auth } from "@/auth"
+import { NextResponse } from "next/server";
 
-export const { GET, POST } = handlers 
+export async function GET() {
+  await auth();
+  
+  return NextResponse.json({ 
+    status: "success", 
+    message: "Auth API route is working" 
+  });
+}
+
+export async function POST() {
+  await auth();
+  
+  return NextResponse.json({ 
+    status: "success", 
+    message: "Auth API route is working" 
+  });
+} 
